9d621264b8384c026275ce306de48d1937eda77e
Musaab Shalek <34851646+MosaabShalek@users.noreply.github.com>
2020-01-09 17:04:15 +0200

    Add files via upload

---
+++ Python_course_code_by_Musab_Schluck.py
@@ -0,0 +1,27 @@
+print("hello World")
+
+print(3 + 2)
+print(3.5 - 2)
+print(3*2)
+print(3/2)                      # when dividing the resulting number is always a float
+print(3//2)
+print(5//2)
+print(3**2)
+print(3%2)
+
+# correct variable names
+a = 5
+b = 1.2
+my_age = 26                     #  snake_case
+myAge = 26                      # camelCase
+number1 = 1e6                # e6 = x10^6
+number2 = 77
+
+# incorrect variable names
+1number = 20                       # starts with a number
+ahmed weight = 95              # has a space
+sarah@ahmed = 10              # has a special chatacter
+sum = 55                                # using a preserved word
+
+
+
